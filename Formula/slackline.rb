b5e8b7e1f1ff49287fc040adae593be1eb595e0e
Corentin REGAL <corentin@huggingface.co>
2026-01-28 16:55:01 +0100

    Use pre-built binaries instead of building from source

--- Formula/slackline.rb
+++ Formula/slackline.rb
@@ -1,15 +1,26 @@
 class Slackline < Formula
   desc "Read-only Slack CLI for AI agents"
   homepage "https://github.com/co42/slackline"
-  url "https://github.com/co42/slackline/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "2634687d0018c7ccff75db8fd1fb6f37b8d860e735968c595a1c4bd4198914ca"
+  version "0.3.0"
   license "MIT"
-  head "https://github.com/co42/slackline.git", branch: "main"
 
-  depends_on "rust" => :build
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/co42/slackline/releases/download/v0.3.0/slackline-v0.3.0-aarch64-apple-darwin.tar.gz"
+      sha256 "f58369c27133956d835008129c0e7104e1c07841a0c52e5b3baeedc36189d4ea"
+    else
+      url "https://github.com/co42/slackline/releases/download/v0.3.0/slackline-v0.3.0-x86_64-apple-darwin.tar.gz"
+      sha256 "51b77cd693f24e0b4f34fbe10f252b2ba64d43956ff1be877cd70d797260939c"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/co42/slackline/releases/download/v0.3.0/slackline-v0.3.0-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "0125a63ee10943023064f6620b62113d3637c87e0dcb3c0860dfe1ab55e15e00"
+  end
 
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "slackline"
   end
 
   test do
